d992a2cad47226401e261e50eda063bc37d9c66a
Glenn E. Bailey III <gbailey@vinatinc.com>
2020-02-24 17:13:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# drone-json
+[![Build Status](https://cloud.drone.io/api/badges/viant/drone-yaml-verify/status.svg)](https://cloud.drone.io/viant/drone-yaml-verify)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://github.com/viant/drone-yaml-verify/blob/master/LICENSE)
+
+Drone plugin to validate YAML files.
+
+## Usage
+
+* `files` An array of file(s)
+
+* `dirs` An array of path(s) that have `.yaml` files
+
+* `debug` if True will print out the results of `yq` along with any plugin parameters 
+
+## Example
+
+```yaml
+kind: pipeline
+name: default
+
+steps:
+
+- name: test
+  image: viant/drone-yaml-verify:latest
+  settings:
+    dirs:
+      - path/to/some/yaml
+      - another/path/to/some/yaml
+    files:
+      - path/to/file.yaml
+      - pathfile.yaml
+```
---
+++ startup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Verify if list of files
+if [ -n "$FILES" ]
+then
+    IFS=","
+    for v in $FILES
+    do
+        if [ -n "$DEBUG" ]
+        then
+            cat "$v" | yq .
+        else
+            cat "$v" | yq . > /dev/null 2>&1
+        fi
+        if [[ $? == 0 ]]
+        then
+            echo "$v : valid"
+        else
+            echo "$v : invalid"
+            exit 1 
+        fi
+   done
+fi
+
+# Verify if list of dirs
+if [ -n "$DIRS" ]
+then
+    IFS=","
+    for d in $DIRS
+    do
+        for v in $d/*
+        do
+            if [ -n "$DEBUG" ]
+            then
+                cat "$v" | yq .
+            else
+                cat "$v" | yq . > /dev/null 2>&1
+            fi
+            if [[ $? == 0 ]]
+            then
+                echo "$v : valid"
+            else
+                echo "$v : invalid"
+                exit 1 
+            fi
+        done
+   done
+fi
